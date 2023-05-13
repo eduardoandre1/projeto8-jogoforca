1aac097d247c5ff4822a91c233f9718405df6192
Eduardo André <eduardoandre418@gmail.com>
2023-05-13 20:39:28 -0300

    alterações de palavras para data test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import Jogo from "./Jogo"
 import AlfabetoHTML from "./Letras";
-import Palavras from "./Palavras";
+import palavras from "./Palavras";
 function App(props) {
-  const palavra = Palavras[Math.floor(Math.random()*Palavras.length)]
+  const palavra = palavras[Math.floor(Math.random()*palavras.length)]
   const [tentativa,setTentativa] = useState(0)
   const[codigo,setCodigo] = useState(palavra.split(""))//palvra secreta
   const [secret,setSecret] = useState("")//palavar mostrada
@@ -13,8 +13,8 @@ function App(props) {
     codigo:codigo,secret:secret,}
   return (
     <div class='container'>
-      <Jogo codigo={codigo} setCodigo={setCodigo} Palavras={Palavras} letras_usadas={letras_usadas} secret={secret} setSecret={setSecret} tentativa ={tentativa} setTentativa ={setTentativa} setIniciar={setIniciar} iniciar={iniciar}/>
-      <AlfabetoHTML Palavras={Palavras} setIniciar={setIniciar} tentativa={tentativa} setTentativa={setTentativa}codigo={codigo} secret={secret} setSecret={setSecret} letras_usadas={letras_usadas} setUsadas={setUsadas}  iniciar={iniciar}/>    
+      <Jogo codigo={codigo} setCodigo={setCodigo} Palavras={palavras} letras_usadas={letras_usadas} secret={secret} setSecret={setSecret} tentativa ={tentativa} setTentativa ={setTentativa} setIniciar={setIniciar} iniciar={iniciar}/>
+      <AlfabetoHTML Palavras={palavras} setIniciar={setIniciar} tentativa={tentativa} setTentativa={setTentativa}codigo={codigo} secret={secret} setSecret={setSecret} letras_usadas={letras_usadas} setUsadas={setUsadas}  iniciar={iniciar}/>    
     </div>
   );
 }
