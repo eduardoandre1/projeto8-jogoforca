b0d1239fbac01cf09bc1f81fb3ae90d3c904bed5
Eduardo-dev <128654896+eduardoandre1@users.noreply.github.com>
2023-05-15 20:29:30 -0300

    valor certo quando perder

--- src/Jogo.js
+++ src/Jogo.js
@@ -56,6 +56,7 @@ export default function Jogo(props){
                         >
                             Escolher Palavra
                     </button>
+                    <h2 className="secret">{props.hevenorhell==false?props.codigo:""}
                     <h2 className="secret">
                         tentativas :{props.iniciar==true?6-props.tentativa:""}
                     </h2>
