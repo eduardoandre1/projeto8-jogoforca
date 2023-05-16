92564fb3051a8f5f21a78c8cdce35b6ec1e13491
Eduardo-dev <128654896+eduardoandre1@users.noreply.github.com>
2023-05-16 16:39:39 -0300

    sem gabarito

--- src/Jogo.js
+++ src/Jogo.js
@@ -58,9 +58,6 @@ export default function Jogo(props){
                             Escolher Palavra
                     </button>
                     <h2 className="secret">
-                        {props.heavenorhell==false&&props.iniciar!=true?props.codigo:""}
-                    </h2>
-                    <h2 className="secret">
                         tentativas :{props.iniciar===true?6-props.tentativa:""}
                     </h2>
                     <h1 className={classe1}>
